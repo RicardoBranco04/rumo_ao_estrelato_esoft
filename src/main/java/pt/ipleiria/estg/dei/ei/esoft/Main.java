55fcd8d799008c1580e31ab774a643db1f0184ea
Tiago Silva <2221428@my.ipleiria.pt>
2024-04-23 11:51:20 +0100

    SIIIUU

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Rumo ao Estrelato!");
         System.out.println("Ola toze");
+        System.out.println("batatas");
     }
 }
