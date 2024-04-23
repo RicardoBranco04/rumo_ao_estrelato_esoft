a6b724a777e22c7a41c9c449335fbeb6829e2aeb
Ricardo Branco <2221465@my.ipleiria.pt>
2024-04-23 13:02:59 +0100

    Comit do Tozé

--- src/main/java/Pagina_Inicial.java
+++ src/main/java/Pagina_Inicial.java
@@ -1,8 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Pagina_Inicial {
+public class Pagina_Inicial extends JFrame{
     private JPanel painelPrincipal;
-    private JLabel nomeClube;
-    private JLabel logo;
+    private JLabel labelNomeClube;
+    private JLabel labelLogo;
     private JButton buttonMenu;
+    private JLabel labelSaldo;
+
+    public Pagina_Inicial(String title) {
+        super(title);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+        pack();
+    }
+    public static void main(String[] args) {
+        new Pagina_Inicial("Rumo ao Estrelato!!!").setVisible(true);
+    }
+
 }
