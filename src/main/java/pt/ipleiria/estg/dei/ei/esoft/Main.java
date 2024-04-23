cd5583e0d54346b5cf822678c34aad807e4fad3f
João Febra <joaodinisfebra@gmail.com>
2024-04-23 13:12:20 +0100

    puta fui eu

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
@@ -1,5 +1,7 @@
 package pt.ipleiria.estg.dei.ei.esoft;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Rumo ao Estrelato!");
